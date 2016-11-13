fbf4c0e81686aab51ede468449dca2f68d376333
artem-p <artempugachev1@gmail.com>
2016-11-13 21:45:55 +0300

    test redirect

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,13 @@
 # todo http://flask.pocoo.org/docs/0.11/testing/
 
-import flaskr
+import src.rest as rest
 
-test_app = flaskr.a
+rest.app.config['TESTING'] = True
+test_app = rest.app.test_client()
 
-def test_get_current():
+
+def test_redirect():
+    resp = test_app.get('/')
+    assert resp.status_code == 302
+    assert resp.location == 'http://localhost/api/1.0/current'
+    pass
